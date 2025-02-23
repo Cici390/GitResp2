8faa74f1cf2d7209ca262a874e1ddfc0251ee1d5
Cici390 <2628716004@qq.com>
2025-02-23 12:05:59 +0800

    在Person中添加了age...

---
+++ TestModule01/src/com/glz/Person.java
@@ -0,0 +1,11 @@
+package com.glz;
+
+/**
+ * @Auther: gelongze
+ * @Date: 2025/2/23 - 02 - 23 - 12:04
+ * @Description: com.glz
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
